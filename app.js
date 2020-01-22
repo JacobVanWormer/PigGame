9692545e55adf075b48da77fc9bda15689cbb7ae
Jacob VanWormer <sharpiejacob@gmail.com>
2020-01-22 07:08:56 -0800

    Functionality of the ROLL DICE button

---
+++ app.js
@@ -0,0 +1,28 @@
+
+var scores, roundScore, activePlayer;
+
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 1;
+
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+// Roll Dice Button 
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+});
+
+
+
